5f5f3a5a238431962fa4ab83a522280b1e13b307
Caio Luiz Cardoso Oliveira <ccardoso210@gmail.com>
2022-09-27 17:52:53 -0300

    commit 1

---
+++ src/mocks/jogos.js
@@ -0,0 +1,46 @@
+const jogos = [
+  {
+    titulo: "God Of War Ragnarok",
+    descricao:
+      "Junte-se a Kratos e Atreus em uma jornada mítica à procura de respostas na iminência do Ragnarök. Juntos, pai e filho arriscarão tudo viajando por cada um dos Nove Reinos.",
+    foto: require("assets/images/acai-com-leite-condensado.png").default,
+    preco: 299.99,
+    genero: "Fantasia/Ação",
+    distribuidora: "Sony",
+  },
+  {
+    titulo: "Gran Turismo 7",
+    descricao:
+      "Curta a experiência completa do verdadeiro simulador de corridas... com 25 anos de estrada.",
+    foto: require("assets/images/banana-com-nutella.png").default,
+    preco: 197.93,
+    genero: "Corrida",
+    distribuidora: "Sony",
+  },
+  {
+    titulo: "Cyberpunk 2077",
+    descricao:
+      "Cyberpunk 2077 é um RPG de ação e aventura em mundo aberto ambientado na megalópole de Night City, onde você joga como um mercenário cyberpunk envolvido numa intensa luta pela sobrevivência.",
+    foto: require("assets/images/acai-com-leite-condensado.png").default,
+    preco: 124.75,
+    genero: "FPS/RPG",
+    distribuidora: "CD Projekt Red",
+  },
+  {
+    titulo: "Dragon Ball Z: Kakarot",
+    descricao:
+      "Viva novamente a história de Goku e outros Guerreiros Z em Dragon Ball Z: KAKAROT! Além das batalhas épicas, sinta como é a vida no mundo de Dragon Ball Z.",
+    foto: require("assets/images/acai-com-leite-condensado.png").default,
+    preco: 249.90,
+    genero: "Luta/RPG",
+    distribuidora: "Bandai Namco",
+  },
+  {
+    titulo: "FIFA 23",
+    descricao:
+      "FIFA 23 proporciona ainda mais ação e realismo do futebol aos campos do Jogo de Todo Mundo.",
+    foto: require("assets/images/acai-com-leite-condensado.png").default,
+    preco: 338.90,
+    genero: "Esportes/Futebol",
+    distribuidora: "EA Sports",
+  }
