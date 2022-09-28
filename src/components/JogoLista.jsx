089cc5ffe802803f656975dad53f9a9c165c1abd
Caio Luiz Cardoso Oliveira <ccardoso210@gmail.com>
2022-09-28 19:21:17 -0300

    botão remover

--- src/components/JogoLista.jsx
+++ src/components/JogoLista.jsx
@@ -9,10 +9,26 @@ function JogoLista() {
     const jogo = { [jogoIndex]: Number(jogoSelecionado[jogoIndex] || 0) + 1 };
     setjogoSelecionado({ ...jogoSelecionado, ...jogo });
   };
+
+  const removerItem = (jogoIndex) => {
+    const jogo = { [jogoIndex]: Number(jogoSelecionado[jogoIndex] || 0) - 1 };
+    setjogoSelecionado({ ...jogoSelecionado, ...jogo });
+  };
+
+  const badgeCounter = (canRender, index) =>
+    Boolean(canRender) && (
+      <span className="JogoListaItem__badge"> {jogoSelecionado[index]} </span>
+    );
+
+  const removeButton = (canRender, index) =>
+    Boolean(canRender) && (
+      <button className="Acoes__remover" onClick={()=> removerItem(index)}>remover</button>
+    );
   return (
     <div className="JogoLista">
       {jogos.map((jogo, index) => (
         <div className="JogoListaItem" key={`JogoListaItem-${index}`}>
+          {badgeCounter(jogoSelecionado[index], index)}
           <div>
             <div className="JogoListaItem__titulo">{jogo.titulo}</div>
             <div className="JogoListaItem__preco">
@@ -28,14 +44,16 @@ function JogoLista() {
             <div className="JogoListaItem__distribuidora">
               {jogo.distribuidora}
             </div>
-            <span className="JogoListaItem__badge"> {jogoSelecionado[index] || 0} </span>
             <div className="JogoListaItem__acoes Acoes">
               <button
-                className="Acoes__adicionar Acoes__adicionar--preencher"
+                className={`Acoes__adicionar ${
+                  !jogoSelecionado[index] && "Acoes__adicionar--preencher"
+                }`}
                 onClick={() => adicionarItem(index)}
               >
                 Adicionar
               </button>
+              {removeButton(jogoSelecionado[index],index)}
             </div>
           </div>
         </div>
